44b25ac901bd0fc92c1490b212f450adb816ede3
Lucas Selfslagh <lucas.selfslagh@inthepocket.mobi>
2019-01-10 11:46:07 +0100

    🍝⏳

--- ikwilnaarhuis/cli.py
+++ ikwilnaarhuis/cli.py
@@ -20,7 +20,7 @@ def main():
   now = datetime.datetime.now()
   parser = argparse.ArgumentParser(description="IK WIL NAAR HUIS")
   parser.add_argument('-t', '--time', nargs="+", metavar='T', type=int, help='the time you started working in hours, optional')
-  parser.add_argument("-l", "--lunch", metavar='L', type=int, required=False,  dest="break", help="Enter your lunch break in minutes.")
+  parser.add_argument("-l", "--lunch", metavar='L', type=int, required=False,  dest="lunch", help="Enter your lunch break in minutes.")
 
   args = parser.parse_args()
   
@@ -43,13 +43,22 @@ def main():
     # second integer is always interpreted as minutes
     # if not second integer, we use minutes = 0
 
+    # -l or --lunch is optional parameter for specifing your lunch break duration, 60 minutes default. 
+  
 
+  if args.lunch == None:
+    print("default lunch break of 60 min")
+    pass
+  
+  else:
+    print("lunch break with user input {}".format(args.lunch))
+    pass
 
   enter = constructDate(start_hour, minutes)
   
   leave = calculateLeave(enter)
 
-  # -P is optional parameter for specifing your lunch break duration, 60 minutes default. 
+
   
   print("You are allowed to leave at " + leave.strftime("%H:%M" + " my nigga"))
 
